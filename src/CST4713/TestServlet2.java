3b8da1fe085d047a91ed707c5904913bf19e2b5a
Serhiy Yarosh <m7grey@gmail.com>
2018-02-06 21:11:13 -0500

    Initial commit

---
+++ src/CST4713/TestServlet2.java
@@ -0,0 +1,28 @@
+package CST4713;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import coreservlets.*;
+
+/** Simple servlet for testing. Makes use of a helper class. */
+
+@WebServlet("/getFormData")
+public class TestServlet2 extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String title = "Halo";
+        out.println
+                (ServletUtilities.headWithTitle(title) +
+                        "<body bgcolor=\"#fdf5e6\">\n" +
+                        "<h1>" + title + "</h1>\n" +
+                        "First Name: "+ request.getParameter("firstName")+"\n"+
+                        "Last Name: "+ request.getParameter("lastName")+
+                        "</body></html>");
+    }
+}
