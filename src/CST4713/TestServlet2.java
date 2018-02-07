309d049d23ac34b6bdb9eb4348761bafc5b06df2
Serhiy Yarosh <m7grey@gmail.com>
2018-02-06 21:26:02 -0500

    Post - Loop - parse the data

--- src/CST4713/TestServlet2.java
+++ src/CST4713/TestServlet2.java
@@ -1,18 +1,22 @@
 package CST4713;
 
 import java.io.*;
+import java.util.Enumeration;
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
+
 import coreservlets.*;
 
-/** Simple servlet for testing. Makes use of a helper class. */
+/**
+ * Simple servlet for testing. Makes use of a helper class.
+ */
 
 @WebServlet("/getFormData")
 public class TestServlet2 extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request,
-                      HttpServletResponse response)
+                       HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -20,9 +24,18 @@ public class TestServlet2 extends HttpServlet {
         out.println
                 (ServletUtilities.headWithTitle(title) +
                         "<body bgcolor=\"#fdf5e6\">\n" +
-                        "<h1>" + title + "</h1>\n" +
-                        "First Name: "+ request.getParameter("firstName")+"\n"+
-                        "Last Name: "+ request.getParameter("lastName")+
-                        "</body></html>");
+                        "<h1>" + title + "</h1>\n");
+
+                        Enumeration <String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = (String) paramNames.nextElement();
+            out.println("<BR>" + paramName+ ":"+ request.getParameter(paramName));
+        }
+
+
+
+//                        "First Name: "+ request.getParameter("firstName")+"\n"+
+//                        "Last Name: "+ request.getParameter("lastName")+
+        out.println( "</body></html>");
     }
 }
